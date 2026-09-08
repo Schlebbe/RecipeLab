c72acfd93cc79d9e677beb563188daee6715d5a2
Sebastian Enerstrand <sebastian@temtron.se>
2026-09-08 10:43:12 +0200

    Add logout endpoint

---
+++ RecipeLab/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RecipeLab.Infrastructure.Identity;
+
+namespace RecipeLab.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AuthController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [Authorize]
+        [HttpPost("logout")]
+        public async Task<IActionResult> LogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+
+            return NoContent();
+        }
+    }
+}
